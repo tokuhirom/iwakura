85e205ec4ea6a8d4c73716af21b6f06486d1a37e
tokuhirom <tokuhirom@gmail.com>
2013-01-13 17:15:14 +0900

    refactor the test case

--- t/01_simple.rb
+++ t/01_simple.rb
@@ -3,27 +3,35 @@ require 'minitest/autorun'
 require './iwakura'
 
 class TestSimple < MiniTest::Unit::TestCase
-  def test_simple
-    tmpl = Iwakura.new()
-    assert_equal tmpl.render_string('helloHello: [% 4 %]'), "helloHello: #{4}"
-    assert_equal tmpl.render_string('helloHello: [% 3+2 %]'), "helloHello: 5"
-    assert_equal tmpl.render_string('helloHello: [% 3-2 %]'), "helloHello: 1"
-    assert_equal tmpl.render_string('helloHello: [% 3-2-4 %]'), "helloHello: #{3-2-4}"
-    assert_equal tmpl.render_string('helloHello: [% 3*4*8 %]'), "helloHello: #{3*4*8}"
-    # tmpl.enable_disasm = true
-    assert_equal tmpl.render_string('helloHello: [% 8/2/2 %]'), "helloHello: #{8/2/2}"
-    assert_equal tmpl.render_string('helloHello: [% 8/2/2 %]'), "helloHello: #{8/2/2}"
-    assert_equal tmpl.render_string('helloHello: [% nil %]'), "helloHello: #{nil}"
+  def setup
+    @tmpl = Iwakura.new()
+  end
 
-    assert_equal tmpl.render_string('helloHello:[% IF nil %]e[% END %]'), "helloHello:"
-    assert_equal tmpl.render_string('helloHello:[% IF nil %]e[% END %]P'), "helloHello:P"
-    assert_equal tmpl.render_string('helloHello:[% IF 1 %]e[% END %]'), "helloHello:e"
-    assert_equal tmpl.render_string('helloHello:[% IF 1 %]e[% END %]E'), "helloHello:eE"
-    assert_equal tmpl.render_string('helloHello:[% IF 1 %]e[% IF 3 %]o[% END %]f[% END %]E'), "helloHello:eofE"
-    assert_equal tmpl.render_string('helloHello:[% IF 1 %]e[% IF nil %]o[% END %]f[% END %]E'), "helloHello:efE"
-    assert_equal tmpl.render_string('helloHello:[% IF nil %]e[% IF 1 %]o[% END %]f[% END %]E'), "helloHello:E"
-    assert_equal tmpl.render_string('[% [1,2,3] %]'), "[1, 2, 3]"
-    assert_equal tmpl.render_string('[% [] %]'), "[]"
-    assert_equal tmpl.render_string('[% [1,] %]'), "[1]"
+  def test_if
+    assert_equal @tmpl.render_string('helloHello:[% if nil %]e[% end %]'), "helloHello:"
+    assert_equal @tmpl.render_string('helloHello:[% if nil %]e[% end %]P'), "helloHello:P"
+    assert_equal @tmpl.render_string('helloHello:[% if 1 %]e[% end %]'), "helloHello:e"
+    assert_equal @tmpl.render_string('helloHello:[% if 1 %]e[% end %]E'), "helloHello:eE"
+    assert_equal @tmpl.render_string('helloHello:[% if 1 %]e[% if 3 %]o[% end %]f[% end %]E'), "helloHello:eofE"
+    assert_equal @tmpl.render_string('helloHello:[% if 1 %]e[% if nil %]o[% end %]f[% end %]E'), "helloHello:efE"
+    assert_equal @tmpl.render_string('helloHello:[% if nil %]e[% if 1 %]o[% end %]f[% end %]E'), "helloHello:E"
+  end
+
+  def test_simple
+    assert_equal @tmpl.render_string('helloHello: [% 4 %]'), "helloHello: #{4}"
+    assert_equal @tmpl.render_string('helloHello: [% 3+2 %]'), "helloHello: 5"
+    assert_equal @tmpl.render_string('helloHello: [% 3-2 %]'), "helloHello: 1"
+    assert_equal @tmpl.render_string('helloHello: [% 3-2-4 %]'), "helloHello: #{3-2-4}"
+    assert_equal @tmpl.render_string('helloHello: [% 3*4*8 %]'), "helloHello: #{3*4*8}"
+    # @tmpl.enable_disasm = true
+    assert_equal @tmpl.render_string('helloHello: [% 8/2/2 %]'), "helloHello: #{8/2/2}"
+    assert_equal @tmpl.render_string('helloHello: [% 8/2/2 %]'), "helloHello: #{8/2/2}"
+    assert_equal @tmpl.render_string('helloHello: [% nil %]'), "helloHello: #{nil}"
+  end
+
+  def test_array
+    assert_equal @tmpl.render_string('[% [1,2,3] %]'), "[1, 2, 3]"
+    assert_equal @tmpl.render_string('[% [] %]'), "[]"
+    assert_equal @tmpl.render_string('[% [1,] %]'), "[1]"
   end
 end
